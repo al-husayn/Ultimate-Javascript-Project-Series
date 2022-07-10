678f2e1f0ff79990c9a06c60fb024e335d07ff71
Al-Hussein Abubakar <82461094+al-husayn@users.noreply.github.com>
2022-07-10 11:17:18 +0000

    Day 6: Card Expandable and Collapsible updated

--- Day 6. Expandable and collapsible/app.js
+++ Day 6. Expandable and collapsible/app.js
@@ -3,18 +3,18 @@
 let cards = document.querySelectorAll(".card");
 
 
-for (const element of cards) {
-    element.addEventListener("click", ()=> {
+for (const card of cards) {
+    card.addEventListener("click", ()=> {
         removeActiveClass();
         // Add the active class from all cards
-        element.classList.add("active");
+        card.classList.add("active");
     });
 }
 
 // Remove the active class from all cards
 
 const  removeActiveClass =()=> {
-    for (const element of cards) {
-        element.classList.remove("active");
+    for (const card of cards) {
+        card.classList.remove("active");
     }
 }
